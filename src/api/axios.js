7ba6cdf550feaae22c8b52078accb831dcd728d9
Mehak Malhotra <mehakmalhotra200479@gmail.com>
2026-06-17 16:25:18 +0530

    connect render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-backend-ztnv.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
